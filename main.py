4bd9deb03beaa90ec83158ea5224864a42b60a04
Ido Harlev <Harlev.ido@gmail.com>
2022-11-13 18:52:07 +0200

    second Version

--- main.py
+++ main.py
@@ -9,14 +9,3 @@ Mail = 'Ido@stelosoties.com'
 
 
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('Ido')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
