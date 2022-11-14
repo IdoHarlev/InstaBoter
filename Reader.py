0f7f123496e05c104ba41e02ad74207de16e8fb6
Ido Harlev <Harlev.ido@gmail.com>
2022-11-14 16:28:39 +0200

    last step in reading: put it in the right tensors

--- Reader.py
+++ Reader.py
@@ -1,22 +1,41 @@
-import pandas
+import pandas as pd
 import numpy
 import torch
 from PIL import Image
 import glob
 import os
 
-rootdir ='/Users/ido/Desktop/CoachAI/InstaBoter/rotemsela1'
+rootdir ='/Users/ido/Desktop/CoachAI/InstaBoter/idoharlev'
+rootdir1 = '/Users/ido/Desktop/CoachAI/Emitions/test'
+
 
 def get_jpg_from_main_dir(mainroot):
+     subdirs = [x[1] for x in os.walk(mainroot)]
+     print(subdirs[0])
+     for i,k in enumerate(subdirs[0]):
+          root = mainroot + '/' + str(k) + '/*.jpg'
+          jpgFilenamesList = glob.glob(root)
+          for j in jpgFilenamesList:
+               print(j,k)
+               image = Image.open(j)
+               # image.show()
+
+
+     '''
      for subdir, dirs, files in os.walk(mainroot):
           print(subdir)
-          jpgFilenamesList = glob.glob(str(subdir) + '/*.jpg')
+
+          try:
+               jpgFilenamesList = glob.glob(str(subdir) + '/*.jpg')
+          except:
+               jpgFilenamesList = glob.glob(str(subdir) + '/*.png')
           for i in jpgFilenamesList:
                image = Image.open(i)
                #image.show()
+               '''
 
-'''
-subdirs = [x[1] for x in os.walk(rootdir)]
-print(subdirs[0][0])
-'''
+get_jpg_from_main_dir(rootdir)
+
+
+
 
