888d6fce5687dfbb05416811f6db8ab86fdd252b
Ido Harlev <Harlev.ido@gmail.com>
2022-11-14 10:53:54 +0200

    second Version

--- Reader.py
+++ Reader.py
@@ -1,4 +1,22 @@
 import pandas
 import numpy
 import torch
+from PIL import Image
+import glob
+import os
+
+rootdir ='/Users/ido/Desktop/CoachAI/InstaBoter/rotemsela1'
+
+def get_jpg_from_main_dir(mainroot):
+     for subdir, dirs, files in os.walk(mainroot):
+          print(subdir)
+          jpgFilenamesList = glob.glob(str(subdir) + '/*.jpg')
+          for i in jpgFilenamesList:
+               image = Image.open(i)
+               #image.show()
+
+'''
+subdirs = [x[1] for x in os.walk(rootdir)]
+print(subdirs[0][0])
+'''
 
